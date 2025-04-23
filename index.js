b434ca147b5b51e083ea8952e3bf52fbeb69d535
Casper Gamborg <Gamborgc@gmail.com>
2025-04-23 12:47:11 +0200

    Most of the logic is setup. Next up is a bit of styling and making buttons work

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
+/* LOGIC */
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = function () {
-    return `You have read ${this.title}, and it was by ${this.author}. The book was ${pages}`;
-  };
+
+  addBookToLibrary(this.title, this.author, this.pages, this.read);
 }
 
-let book1 = new Book("LOTR", "JJR", 225);
+function addBookToLibrary(title, author, pages, read) {
+  let newBook = {
+    title: title,
+    author: author,
+    pages: pages,
+    read: read,
+    id: crypto.randomUUID(),
+  };
+  myLibrary.push(newBook);
+}
 
-console.log(book1.info());
+let book1 = new Book("Lord of the Rings", "JJR", 225, true);
+let book2 = new Book(
+  "The Girl With the Dragon Tattoo",
+  "Stieg Larsson",
+  480,
+  false
+);
+let book3 = new Book(
+  "The Curious Incident of the Dog in the Night-Time",
+  "Mark Haddon",
+  226,
+  false
+);
+
+/* Adding to page */
+
+const bookContainer = document.getElementById("book-container");
+
+for (let element of myLibrary) {
+  let addNewBook = document.createElement("div");
+  addNewBook.innerText = element.title;
+  addNewBook.id = element.id;
+  bookContainer.appendChild(addNewBook);
+}
